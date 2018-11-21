ccade42821c7d94f7c5d656d26d640428b9c23c8
root <root@vultr.guest>
2018-11-21 06:58:26 +0000

    init

--- public/webhook.php
+++ public/webhook.php
@@ -1,32 +1,17 @@
-<?php
-  $target = '/www/wwwroot/redSnail'; // 生产环境web目录
-  //密钥
-  $secret = "Hinterwolf08";
-  //获取GitHub发送的内容
-  $json = file_get_contents('php://input');
-  $content = json_decode($json, true);
-  //github发送过来的签名
-  $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
-  if (!$signature) {
-    return http_response_code(404);
+
+ <?php
+// 与webhook配置相同，为了安全，请设置此参数
+$secret = "Hinterwolf08";
+// 项目路径
+$path = "/www/wwwroot/redSnail";
+// 校验发送位置，正确的情况下自动拉取代码，实现自动部署
+$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+if ($signature) {
+  $hash = "sha1=".hash_hmac('sha1', file_get_contents("php://input"), $secret);
+  if (strcmp($signature, $hash) == 0) {
+    echo shell_exec("cd \ && cd {$path} && git pull 2>&1");
+    exit();
   }
-  list($algo, $hash) = explode('=', $signature, 2);
-  //计算签名
-  $payloadHash = hash_hmac($algo, $json, $secret);
-  // 判断签名是否匹配
-  if ($hash === $payloadHash) {
-    $cmd = "cd $target && git pull";
-    $res = shell_exec($cmd);
-    $res_log = 'Success:'.PHP_EOL;
-    $res_log .= $content['head_commit']['author']['name'] . ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push了' . count($content['commits']) . '个commit：' . PHP_EOL;
-    $res_log .= $res.PHP_EOL;
-    $res_log .= '======================================================================='.PHP_EOL;
-    echo $res_log;
-  } else {
-    $res_log  = 'Error:'.PHP_EOL;
-    $res_log .= $content['head_commit']['author']['name'] . ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push了' . count($content['commits']) . '个commit：' . PHP_EOL;
-    $res_log .= '密钥不正确不能pull'.PHP_EOL;
-    $res_log .= '======================================================================='.PHP_EOL;
-    echo $res_log;
-  }
+}
+http_response_code(404);
 ?>
