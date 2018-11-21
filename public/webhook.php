71b2ea170ab2cb780c11a6c9542e68c6e6d025eb
root <root@vultr.guest>
2018-11-21 07:05:54 +0000

    init

--- public/webhook.php
+++ public/webhook.php
@@ -5,7 +5,7 @@ $secret = "Hinterwolf08";
 // 项目路径
 $path = "/www/wwwroot/redSnail";
 // 校验发送位置，正确的情况下自动拉取代码，实现自动部署
-$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+$signature = $_SERVER['X_HUB_SIGNATURE'];
 if ($signature) {
   $hash = "sha1=".hash_hmac('sha1', file_get_contents("php://input"), $secret);
   if (strcmp($signature, $hash) == 0) {
