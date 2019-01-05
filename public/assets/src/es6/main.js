56df74d6ab8559c4dc63f250c0ba1744b0e72f02
jonny <2248977158@qq.com>
2019-01-05 14:52:56 +0800

    header

---
+++ public/assets/src/es6/main.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+  $('#Logo_Group').addClass('active');
+  $('#Logo_Group').find('use').toggleClass('active');
+}
--- public/dist/js/main.js
+++ public/dist/js/main.js
@@ -1,1 +1,6 @@
-"use strict";
+'use strict';
+
+window.onload = function () {
+  $('#Logo_Group').addClass('active');
+  $('#Logo_Group').find('use').toggleClass('active');
+};
