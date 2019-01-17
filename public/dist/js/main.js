d0d8e6f95c35015755f28c7c87b2058b25081087
jonny <2248977158@qq.com>
2019-01-17 16:26:00 +0800

    hamburger menu

--- public/dist/js/main.js
+++ public/dist/js/main.js
@@ -1,6 +1,10 @@
 'use strict';
 
 window.onload = function () {
-  $('#Logo_Group').addClass('active');
-  $('#Logo_Group').find('use').toggleClass('active');
-};
+	$('#Logo_Group').addClass('active');
+	$('#Logo_Group').find('use').toggleClass('active');
+};(function ($) {
+	$('.rs-hamburger-wrapper').on('click', function () {
+		$(this).toggleClass('dropDown');
+	});
+})(jQuery);
