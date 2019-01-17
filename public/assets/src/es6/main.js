5f6dee3d0fec4ff15b50f64b13c794d1ed4af958
jonny <2248977158@qq.com>
2019-01-17 16:24:11 +0800

    hamburger

--- public/assets/src/es6/main.js
+++ public/assets/src/es6/main.js
@@ -2,3 +2,9 @@ window.onload = function() {
   $('#Logo_Group').addClass('active');
   $('#Logo_Group').find('use').toggleClass('active');
 }
+
+;(function($) {
+	$('.rs-hamburger-wrapper').on('click', function() {
+		$(this).toggleClass('dropDown');
+	})
+})(jQuery)
